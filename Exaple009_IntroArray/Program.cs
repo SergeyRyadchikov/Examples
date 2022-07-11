cb54b267b3abe41bcc9bb528664ec1bae1cfe759
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-11 22:56:19 +0300

    add Example009

---
+++ Exaple009_IntroArray/Program.cs
@@ -0,0 +1,35 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//                      индексы массива    
+//               0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 13, 41, 15, 61, 17, 18, 19 };      // Так выглядит массив в С#
+
+array[0] = 43;                    // Так можно обратиться к элементу с индексом в данном случае 0 и записать туда новое значение
+
+Console.WriteLine(array[0]);        // Так можно вывести значение элемента с индексом, в данном случае 0 
+// # Первый вариант решения с помощью функции
+
+//int max1 = Max(a1, b1, c1);                     
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+// # Второй вариант решения с помощью функции
+
+//int max = Max(
+//                Max(a1, b1, c1),
+//                Max(a2, b2, c2),
+//                Max(a3, b3, c3)
+//             );
+
+// # Третий вариант решения с помощью функции
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
